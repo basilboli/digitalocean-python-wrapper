9f33b05af5b33ac28c0a010afc2db661ab05db15
basilboli <basilboli@gmail.com>
2014-07-03 15:30:06 +0200

    better interfaces

--- fabfile.py
+++ fabfile.py
@@ -12,13 +12,14 @@ API_DO_REGIONS = "https://api.digitalocean.com/regions?client_id="+CLIENT_ID+"&a
 API_DO_SIZES = "https://api.digitalocean.com/sizes?client_id="+CLIENT_ID+"&api_key="+API_KEY
 API_DO_DROPLET_ACTION = "https://api.digitalocean.com/droplets/{0}/{1}?client_id="+CLIENT_ID+"&api_key="+API_KEY
 API_DO_DROPLET_NEW = "https://api.digitalocean.com/droplets/new?client_id="+CLIENT_ID+"&api_key="+API_KEY+"&name={0}&size_id={1}&image_id={2}&region_id={3}"
+API_DO_DROPLET_DESTROY = "https://api.digitalocean.com/droplets/destroy?client_id="+CLIENT_ID+"&api_key="+API_KEY+"&name={0}&size_id={1}&image_id={2}&region_id={3}"
 
 
 # 2 - Amsterdam 1, 1 - New York 1, 3 - San Francisco 1, 4 -  New York 2, 5- Amsterdam 2
 DEFAULT_REGION_ID = 5
 # 62 - 2GB, 63 - 1GB, 66 - 500Mb, 64 - 4GB, ...
 DEFAULT_SIZE_ID = 66
-
+DEFAULT_NODE_NAME="node"
 
 
 @task
@@ -52,56 +53,71 @@ def sizes():
     return d
 
 @task
-def show(command=None):
-    """ Show information about created droplets"""
+def droplets(name=None):
+    """ List existing droplets matching given name """
 
-    print "Showing droplets ..."
-    d = get(API_DO_DROPLETS)
-    for droplet in d.get("droplets"):
-        print "{0} ----> {1},{2},{3}".format(droplet.get("name"), droplet.get("ip_address"), droplet.get("id"),
-                                             droplet.get("status"))
-        if (command is not None):
-            pass
+    print "Getting droplets ..."
+    response = get(API_DO_DROPLETS)
+    droplets = response.get("droplets")
+
+    # filtering by name
+    if name :
+        droplets_filtered = [d for d in droplets if name in d.get("name")]
+        droplets = droplets_filtered
+
+    for d in droplets:
+        print "{0}->{1},{2},{3}".format(d.get("name"), d.get("ip_address"), d.get("id"),d.get("status"))
+
+    print "Total droplets found {0}".format(len(droplets))
+    return droplets
 
 @task
-def action(action,name="node"):
+def action(action,name=DEFAULT_NODE_NAME):
     """ Do action on droplet : ex. reboot| power_off| power_on| shutdown| destroy
         Note : default name is 'node'
     """
-
-    if action not in "reboot,power_off,power_on,shutdown,destroy":
+    if action not in "reboot,power_off,power_on,shutdown":
         print "Action is unknown : ", action
         return
 
-    print "Getting droplets ..."
-    d = get(API_DO_DROPLETS)
+    ds = droplets(name)
 
-    for droplet in d.get("droplets"):
-        if (name in droplet.get("name")):
-            print "{0}, id={1}".format(droplet.get("name"), droplet.get("id"))
-            call = API_DO_DROPLET_ACTION.format(droplet.get("id"), action)
-            print "Calling", call
-            r = requests.get(call)
-            print 'Call status : ', r.status_code, r.text
+    for d in ds:
+        print action+"-ing item :{0}, id={1}".format(d.get("name"), d.get("id"))
+        call = API_DO_DROPLET_ACTION.format(d.get("id"), action)
+        print "Calling", call
+        r = requests.get(call)
+        print 'Call status : ', r.status_code, r.text
 
 @task
-def spinup(n=5,image_name="node",region=DEFAULT_REGION_ID,size=DEFAULT_SIZE_ID):
+def spinup(name=DEFAULT_NODE_NAME,n=5,region=DEFAULT_REGION_ID,size=DEFAULT_SIZE_ID):
     """spinup(name,n): spin ups N nodes using snapshot with given name
-    Defaults : default name is 'node', number of instances 5, region  - Amsterdam 2, size - 500Mb instance
+       Defaults : default name is 'node', number of instances 5, region  - Amsterdam 2, size - 500Mb instance
     """
     d = get(API_DO_IMAGES)
     for image in d.get("images"):
-        if (image_name== image.get("name")):
+        if (name== image.get("name")):
             print "Found image {0},id={1}".format(image.get("name"), image.get("id"))
             image_id=image.get("id")
             for i in xrange(int(n)):
-                name = image_name + "{0}".format(i)
-                call = API_DO_DROPLET_NEW.format(name, size, image_id, region)
+                call = API_DO_DROPLET_NEW.format(name + "{0}".format(i), size, image_id, region)
                 print "Calling", call
                 r = requests.get(call)
                 print 'Call status : ', r.status_code, r.text
 
-
+@task
+def destroy(name=DEFAULT_NODE_NAME,n=0,region=DEFAULT_REGION_ID,size=DEFAULT_SIZE_ID):
+    """destroy(name,n): destroys n nodes matching name
+       Defaults : default name is 'node', region  - Amsterdam 2, size - 500Mb instance
+    """
+    ds = droplets(name)
+    ds = ds[0:int(n)]
+    for d in ds:
+        print "destroying item :name={0},id={1}".format(d.get("name"), d.get("id"))
+        call = API_DO_DROPLET_DESTROY.format(d.get("id"))
+        print "Calling", call
+        r = requests.get(call)
+        print 'Call status : ', r.status_code, r.text
 
 def get(url):
     """ Execute GET on given url """
@@ -111,4 +127,3 @@ def get(url):
     # print r.text
     d = json.loads(r.text)
     return d
-
